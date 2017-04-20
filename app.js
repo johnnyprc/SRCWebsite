5eb9dc6e76109a875eda47fcdf18d939b55e2787
johnnyprc <johnnyprc@live.cn>
2017-04-19 23:08:12 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res){
+    res.render('index');
+});
+
+app.listen(3000, function(){
+    console.log('Server started on port 3000...');
+});
